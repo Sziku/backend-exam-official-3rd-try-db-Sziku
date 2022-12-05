9205d3e3b4b246571dc219d2fb29472cef89e60f
Sziku <105989158+Sziku@users.noreply.github.com>
2022-12-05 11:02:02 +0100

    add gerUninqPlane method and pass all test

--- src/main/java/hu/nive/ujratervezes/airport/AirportAdministrator.java
+++ src/main/java/hu/nive/ujratervezes/airport/AirportAdministrator.java
@@ -1,5 +1,10 @@
 package hu.nive.ujratervezes.airport;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class AirportAdministrator {
 
     private final String dbUrl;
@@ -10,5 +15,26 @@ public class AirportAdministrator {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+    }
+
+    public List<String> getUniquePlanes(){
+        List<String> landedPlaneList =  new ArrayList<>();
+
+        final String SQL = "select aircraft_type from airplanes_landed group by aircraft_type order by aircraft_type asc;";
+
+        try(Connection con = DriverManager.getConnection(dbUrl, dbUser, dbPassword)){
+            PreparedStatement st = con.prepareStatement(SQL);
+
+            ResultSet rs = st.executeQuery();
+            while (rs.next()){
+                landedPlaneList.add(rs.getString(1));
+            }
+            Collections.reverse(landedPlaneList);
+            return landedPlaneList;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return  landedPlaneList;
     }
 }
